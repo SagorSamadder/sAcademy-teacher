264f34e374a6bdc24205e3f2bf1731eafbf11a48
Sagor <sagorsamadder51@gmail.com>
2024-01-06 21:17:34 +0600

    setting screen

--- lib/app/settings/view/setting_view.dart
+++ lib/app/settings/view/setting_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 import '../../widgets/drawer.dart';
 
@@ -11,8 +12,65 @@ class SettingScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('setting screen'),
       ),
-      body: Container(
-        color: Colors.greenAccent,
+      body: Center(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            50.heightBox,
+            Container(
+              height: 70,
+              width: context.screenWidth * .70,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: const [
+                  BoxShadow(
+                    blurRadius: 3.0,
+                  ),
+                ],
+              ),
+              child: const Center(
+                child: Text("Privacy policy"),
+              ),
+            ),
+            20.heightBox,
+            Container(
+              height: 70,
+              width: context.screenWidth * .70,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: const [
+                  BoxShadow(
+                    blurRadius: 3.0,
+                  ),
+                ],
+              ),
+              child: const Center(
+                child: Text("Terms & Condition"),
+              ),
+            ),
+            const Spacer(),
+            Container(
+              height: 70,
+              width: context.screenWidth * .70,
+              decoration: BoxDecoration(
+                color: Colors.redAccent,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: const [
+                  BoxShadow(
+                    blurRadius: 3.0,
+                  ),
+                ],
+              ),
+              child: const Center(
+                child: Text("Terms & Condition"),
+              ),
+            ),
+            50.heightBox,
+          ],
+        ),
       ),
       drawer: const MyDrawer(),
     );
