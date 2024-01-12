837d05946d54b85a3dc06da3854c710e2376897a
Sagor <sagorsamadder51@gmail.com>
2024-01-12 13:01:56 +0600

    dashboard design

--- lib/app/home_screen/home_screen.dart
+++ lib/app/home_screen/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 import '../add_question/view/add_question.dart';
 import '../widgets/drawer/drawer.dart';
+import 'widged/containet.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -11,11 +13,54 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        backgroundColor: const Color.fromARGB(255, 204, 217, 253),
+        elevation: 1,
+        title: const Text('Dashboard'),
       ),
       drawer: const MyDrawer(),
-      body: const Center(
-        child: Text('Home Screen'),
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Color(0xffdce5fd),
+        ),
+        child: Column(
+          children: [
+            //showing all numbers
+            10.heightBox,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Sscontainner(
+                  icon: Icons.post_add_outlined,
+                  title: 'Total submited',
+                  count: '100',
+                ),
+                Sscontainner(
+                  icon: Icons.file_download_done_outlined,
+                  title: 'Total Accepted',
+                  count: '50',
+                ),
+              ],
+            ),
+            SizedBox(
+              height: context.screenWidth * .03,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Sscontainner(
+                  icon: Icons.pending_actions_outlined,
+                  title: 'Total pending',
+                  count: '30',
+                ),
+                Sscontainner(
+                  icon: Icons.cancel,
+                  title: 'Total rejected',
+                  count: '20',
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
---
+++ lib/app/home_screen/widged/containet.dart
@@ -0,0 +1,60 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+// ignore: must_be_immutable
+class Sscontainner extends StatelessWidget {
+  var count;
+  IconData? icon;
+  var title;
+  Sscontainner({super.key, this.count, this.icon, this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: context.screenHeight * .14,
+      width: context.screenWidth * .45,
+      decoration: BoxDecoration(
+        color: const Color(0xff3f0097),
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: const [
+          BoxShadow(
+            color: Color.fromARGB(255, 141, 73, 236),
+            blurRadius: 2,
+            offset: Offset(1.0, 2.0),
+          )
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(color: Colors.white, fontSize: 16),
+              ),
+              5.heightBox,
+              Text(
+                count,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          10.widthBox,
+          Icon(
+            icon,
+            color: Colors.white,
+            size: 30,
+          )
+        ],
+      ),
+    );
+  }
+}
