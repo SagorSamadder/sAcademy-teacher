00a88fcf7c80b8b7bbbf947fa14b658204ff6f1a
Sagor <sagorsamadder51@gmail.com>
2024-01-15 14:45:23 +0600

    question details page design

--- lib/app/question_details/view/question_details_screen.dart
+++ lib/app/question_details/view/question_details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class QuestionDetailsScreen extends StatelessWidget {
   // ignore: prefer_typing_uninitialized_variables
@@ -15,8 +16,95 @@ class QuestionDetailsScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Text("${data['question']}"),
-          Text("${data['option 1']}"),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              width: context.screenWidth,
+              padding: EdgeInsets.all(context.screenWidth * .05),
+              decoration: BoxDecoration(
+                color: const Color(0xff4bb050),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(context.screenWidth * .04),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text("Question: ${data['question']}"),
+                  ),
+                  10.heightBox,
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(context.screenWidth * .04),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text("Option 1: ${data['optiono1']}"),
+                  ),
+                  10.heightBox,
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(context.screenWidth * .04),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text("Option 2: ${data['option2']}"),
+                  ),
+                  10.heightBox,
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(context.screenWidth * .04),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text("Option: ${data['option3']}"),
+                  ),
+                  10.heightBox,
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(context.screenWidth * .04),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text("Option 4: ${data['option4']}"),
+                  ),
+                  10.heightBox,
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(context.screenWidth * .04),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text("Correct Answer: ${data['answer']}"),
+                  ),
+                  20.heightBox,
+                  Row(
+                    children: [
+                      SizedBox(
+                          width: context.screenWidth * .4,
+                          child: ElevatedButton(
+                              onPressed: () {}, child: Text("Edit"))),
+                      SizedBox(
+                          width: context.screenWidth * .4,
+                          child: ElevatedButton(
+                              onPressed: () {}, child: Text("Delete"))),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
